0af33664a00924c7b0584844e92480c7a6baea90
Sara <saramateos13@gmail.com>
2022-11-29 12:28:28 +0100

    Start design

--- app/src/main/java/com/example/savethebunny/MainActivity.java
+++ app/src/main/java/com/example/savethebunny/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.savethebunny;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+// Tutorial del juego  --> https://www.youtube.com/watch?v=gfX8UHTpq3o&ab_channel=SandipBhattacharya
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
+    public void startGame(View view) {
+       GameView gameView = new GameView(this);
+       setContentView(gameView);
     }
 }
